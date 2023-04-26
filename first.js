ef7185a47cc2e964adce67c6170ce45ea2fdd0d1
ss123developer <69311204+ss123developer@users.noreply.github.com>
2023-04-26 20:41:00 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import front from './img/front.png'
+function first() 
+{
+    return (
+        <div className='split-right'>
+         <img src={front} alt='front'/>   
+        </div>
+    );
+}
+
+export default first;
